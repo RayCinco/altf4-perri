06f8e26938985aac2a7fbc4b7fb6f167e3bbf341
raycinco <138893281+RayCinco@users.noreply.github.com>
2026-05-08 01:27:50 +0800

    Updated UI

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,12 +1,13 @@
 import DetectorPanel from "@/components/DetectorPanel";
 import HeroHeading from "@/components/Heading";
-
+import DetectorBottomShowcase from "@/components/DetectorBottomShowcase";
 export default function Page() {
   return (
     <>
       <HeroHeading />
       <main className="p-6">
         <DetectorPanel />
+        <DetectorBottomShowcase />
       </main>
     </>
   );
